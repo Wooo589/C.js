ed6e451f75c3800ea7fc0b09f38ce4b0aa701f29
Leandro Almeida <leo.leandro.ars@gmail.com>
2025-10-08 15:12:43 -0300

    feat: reorganiza testes e adicionai mais casos de testes

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,3 @@
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -6,23 +5,24 @@ CYAN='\033[0;36m'
 WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
+TEST_DIR="tests"
 COMPILER_EXEC="./a.out"
 PASSED_COUNT=0
 FAILED_COUNT=0
 TOTAL_TESTS=0
 
-
 echo -e "${WHITE}================ EXECUTANDO TESTES ================${NC}"
 make > /dev/null 2>&1
 
-for file in tests/*.txt; do
+for test_file in $(find ${TEST_DIR} -name "*.txt"); do
     TOTAL_TESTS=$((TOTAL_TESTS + 1))
     
-    base_name=$(basename "$test_file" .txt)
-    echo -e "${CYAN}Testando:${NC} $base_name"
+    display_name=${test_file#"$TEST_DIR/"}
+    echo -e "${CYAN}Testando:${NC} $display_name"
 
-    expected_err_file="${TEST_DIR}/${base_name}.expected.err"
-    expected_out_file="${TEST_DIR}/${base_name}.expected.out"
+    full_base_name="${test_file%.txt}"
+    expected_err_file="${full_base_name}.expected.err"
+    expected_out_file="${full_base_name}.expected.out"
     
     actual_out_file=$(mktemp)
     actual_err_file=$(mktemp)
@@ -65,7 +65,7 @@ for file in tests/*.txt; do
             FAILED_COUNT=$((FAILED_COUNT + 1))
         fi
     else
-        echo -e "  └─ ${YELLOW}[WARN]${NC} Nenhum arquivo .expected.* encontrado para $base_name. Pulando verificação."
+        echo -e "  └─ ${YELLOW}[WARN]${NC} Nenhum arquivo .expected.* encontrado para $display_name. Pulando verificação."
     fi
 
     rm "$actual_out_file" "$actual_err_file"
